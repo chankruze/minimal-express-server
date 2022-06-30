e83851afd99165991099e004b480c4a89b73d3f2
chankruze <chankruze@gmail.com>
2022-06-30 07:26:44 +0530

    fix: testing with jest
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/test/App.test.ts
+++ src/test/App.test.ts
@@ -6,27 +6,28 @@ Copyright (c) geekofia 2022 and beyond
 */
 
 import axios from 'axios'
-import { describe, it } from 'mocha'
-import { expect } from 'chai'
+import { describe, expect, it } from '@jest/globals'
 
 describe('App', () => {
   it('server / should return 200', async () => {
     const response = await axios.get('http://localhost:6767/')
-    expect(response.status).to.equal(200)
+    expect(response.status).toBe(200)
   })
 
   it('server /random/50/50 should return only 50', async () => {
     const response = await axios.get('http://localhost:6767/random/50/50')
-    expect(response.data.randomNo).to.equal(50)
+    expect(response.data.randomNo).toBe(50)
   })
 
   it('server /random/4/5 should return 4 or 5', async () => {
     const response = await axios.get('http://localhost:6767/random/4/5')
-    expect(response.data.randomNo).to.greaterThanOrEqual(4).lessThanOrEqual(5)
+    expect(response.data.randomNo).toBeGreaterThanOrEqual(4)
+    expect(response.data.randomNo).toBeLessThanOrEqual(5)
   })
 
   it('server /random/20/50 should return 20 or 50', async () => {
     const response = await axios.get('http://localhost:6767/random/20/50')
-    expect(response.data.randomNo).to.greaterThanOrEqual(20).lessThanOrEqual(50)
+    expect(response.data.randomNo).toBeGreaterThanOrEqual(20)
+    expect(response.data.randomNo).toBeLessThanOrEqual(50)
   })
 })
