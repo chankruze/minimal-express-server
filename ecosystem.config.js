154951375c638b9b982e5710fe95dcbd439924cd
chankruze <chankruze@gmail.com>
2023-01-02 15:46:00 +0530

    pm2: fix cluster instances not spawning up
    Signed-off-by: chankruze <chankruze@gmail.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,10 @@ Created: Mon Jan 02 2023 14:13:16 GMT+0530 (India Standard Time)
 Copyright (c) geekofia 2023 and beyond
 */
 
-import config from './src/config'
-
 module.exports = {
   apps: [
     {
-      name: `${config.APP_NAME}`,
+      name: 'minimal-express-server',
       script: './dist/index.js',
       instances: 3,
       exec_mode: 'cluster',
